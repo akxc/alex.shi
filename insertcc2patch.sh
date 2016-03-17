e67806ffa53a99da02e885c4e4d69446f170ae11
Alex Shi <alex.shi@linaro.org>
2016-03-17 20:53:48 +0800

    insert2ccpatch.sh: add comments and usage

--- insertcc2patch.sh
+++ insertcc2patch.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 # Copyright Alex Shi <alex.shi@linaro.org>
 #
+# This script will insert the related To:/Cc: list for patches
+# used under the kernel source directory
+# then, we have scripts/get_maintainer.pl
+
 # usage: ./$0 prepared_patch.patch
 
 tmpfile=/tmp/`date +%s`
 tofile=${tmpfile}_to
 ccfile=${tmpfile}_cc
-blacklist="glommer@redhat.com"
+
+# someone don't use the old email address now.
+blacklist="glommer@redhat.com alex.shi@intel.com"
 
 scripts/get_maintainer.pl $@ > $tmpfile
 
 cp $tmpfile $tofile
 cp $tmpfile $ccfile
 
+# left maintainers in tofile and commiters in ccfile
 sed -i -e "/maintainer/d" -e 's/(.*)//g' $ccfile 
 sed -i -e "/commit_signer/d" -e 's/(.*)//g' $tofile
 
@@ -21,16 +28,19 @@ for i in `echo $blacklist`; do
 	sed -i "/$i/d" $tofile
 done
 
+# skip duplicate persons.
 while read x; do
 	sed -i "/$x/d" $ccfile
 done < $tofile
 
+# insert cc list
 while read x;do
 	grep "^Cc: $x" $1 && continue
 	echo "Cc: $x"
 	sed "/Signed-off-by:.*/a Cc: $x" -i $1
 done < $ccfile
 
+# insert to list
 while read x;do
 	grep "^To: $x" $1 && continue
 	echo "To: $x"

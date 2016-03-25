1e6d28ec3a5631210ea6c779710f8849a8bee80f
Alex Shi <alex.shi@linaro.org>
2016-03-25 13:11:48 +0800

    lsk-merge-crond: remove debug mode

--- lsk-merge-crontab.sh
+++ lsk-merge-crontab.sh
@@ -6,5 +6,5 @@ export monitor="alex.shi@linaro.org mark.brown@linaro.org"
 
 git --git-dir=/home/alexs/lsk/kernel/.git remote update &&
 for i in 3.14 3.18 4.1 4.4; do
-	bash -x /home/alexs/lsk/scripts/merge-topic.sh $i &>> ~/lsk/lsk-auto-pick.log
+	/home/alexs/lsk/scripts/merge-topic.sh $i &>> ~/lsk/lsk-auto-pick.log
 done

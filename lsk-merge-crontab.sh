6832c13007c9ebf04cd5615170daeef850ae1792
Alex Shi <alex.shi@linaro.org>
2016-03-28 14:10:06 +0800

    merge-crontab: add date for merge log

--- lsk-merge-crontab.sh
+++ lsk-merge-crontab.sh
@@ -4,7 +4,10 @@ export GIT_WORK_TREE=/home/alexs/lsk/kernel
 export GIT_DIR=$GIT_WORK_TREE/.git/
 export monitor="alex.shi@linaro.org mark.brown@linaro.org"
 
+merge_log=~/lsk/lsk-auto-pick.log
+
 git --git-dir=/home/alexs/lsk/kernel/.git remote update &&
+echo "`date`" >> $merge_log
 for i in 3.14 3.18 4.1 4.4; do
-	/home/alexs/lsk/scripts/merge-topic.sh $i &>> ~/lsk/lsk-auto-pick.log
+	/home/alexs/lsk/scripts/merge-topic.sh $i &>> $merge_log
 done

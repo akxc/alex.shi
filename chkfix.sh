5774aa8064ddf05c4f6b80039c514ba0d618c09c
Alex Shi <alex.shi@linaro.org>
2016-04-20 20:52:54 +0800

    chkfix: add usages and enhance parameters

--- chkfix.sh
+++ chkfix.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # parameter $1, the backported base kernel; $2, upstream branch name or tag
+# Used on backported branch
 #
 # this script will check all cherry pick commits id, to see if it was mentioned
 # in later commit log -- between base kernel and latest upstream.
 
-pickedcommit=`git log $1.. | grep 'cherry picked from commit' | awk  '{print $5}'`
+pickedcommit=`git log ${1}.. | grep 'cherry picked from commit' | awk  '{print $5}'`
 for i in $pickedcommit; do
-	git log --grep=${i:0:7} $1..$2
+	git log --grep=${i:0:7} ${1}..${2}
 done

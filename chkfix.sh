080715b74288dd89e1f16df2cbb3f4ae0eda37e3
Alex Shi <alex.shi@linaro.org>
2016-04-20 20:45:46 +0800

    chkfix: check if there some fix for picked commit in latest upstream
    This script will check all cherry pick commits id, to see if it was
    mentioned in later commit log -- between base kernel and latest upstream.

---
+++ chkfix.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# parameter $1, the backported base kernel; $2, upstream branch name or tag
+#
+# this script will check all cherry pick commits id, to see if it was mentioned
+# in later commit log -- between base kernel and latest upstream.
+
+pickedcommit=`git log $1.. | grep 'cherry picked from commit' | awk  '{print $5}'`
+for i in $pickedcommit; do
+	git log --grep=${i:0:7} $1..$2
+done

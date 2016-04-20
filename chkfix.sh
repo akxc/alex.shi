be36c5af52313d82ac9737c499e67fddc9b05fd7
Alex Shi <alex.shi@linaro.org>
2016-04-20 21:47:02 +0800

    chkfix: get forward order and oneline commit style
    get nature order by --reverse and oneline commit style by --oneline

--- chkfix.sh
+++ chkfix.sh
@@ -5,7 +5,7 @@
 # this script will check all cherry pick commits id, to see if it was mentioned
 # in later commit log -- between base kernel and latest upstream.
 
-pickedcommit=`git log ${1}.. | grep 'cherry picked from commit' | awk  '{print $5}'`
+pickedcommit=`git log --reverse ${1}.. | grep 'cherry picked from commit' | awk  '{print $5}'`
 for i in $pickedcommit; do
-	git log --grep=${i:0:7} ${1}..${2}
+	git log --oneline --grep=${i:0:7} ${1}..${2}
 done

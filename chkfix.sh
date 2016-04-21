359109a4688fd6c2cbdbfba82087c269e1533e6f
Alex Shi <alex.shi@linaro.org>
2016-04-21 10:42:01 +0800

    chkfix: use IFS to get full oneline commit output
    Get full line of commit, include commit title, instead of just ID

--- chkfix.sh
+++ chkfix.sh
@@ -5,14 +5,16 @@
 # this script will check all cherry pick commits id, to see if it was mentioned
 # in later commit log -- between base kernel and latest upstream.
 
+IFS=$'\n'
 pickedcommit=`git log --reverse ${1}.. | grep 'cherry picked from commit' | awk  '{print $5}'`
+
 for i in $pickedcommit; do
 	# any commit mentioned picked commit $i?
-	fixid=`git log --oneline --grep=${i:0:7} ${1}..${2} | awk '{print $1}'`
+	fix=`git log --oneline --grep=${i:0:7} ${1}..${2}`
 
-	for f in $fixid; do
+	for f in $fix; do
 		# did it pick already? otherwise print out the fix commit id.
-		gotfix=`git log --oneline --grep=$f ${1}..`
+		gotfix=`git log --oneline --grep=${f:0:7} ${1}..`
 		[ -z "$gotfix" ] && echo $f
 	done
 done

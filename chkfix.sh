43f884dcd936a228eaea72fad0924303c34a6e1c
Alex Shi <alex.shi@linaro.org>
2016-04-21 10:48:03 +0800

    chkfix: comments correction

--- chkfix.sh
+++ chkfix.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-# parameter $1, the backported base kernel; $2, upstream branch name or tag
-# Used on backported branch
+# parameter $1, the backport base kernel; $2, upstream branch name or tag.
+# Using on backported branch
 #
-# this script will check all cherry pick commits id, to see if it was mentioned
-# in later commit log -- between base kernel and latest upstream.
+# This script will check all cherry picked(-sx) commits id, to see if it was 
+# mentioned/fixed in later commit log -- between base kernel and latest upstream.
 
 IFS=$'\n'
 pickedcommit=`git log --reverse ${1}.. | grep 'cherry picked from commit' | awk  '{print $5}'`
 
 for i in $pickedcommit; do
-	# any commit mentioned picked commit $i?
+	# any commit mentioned the picked commit $i?
 	fix=`git log --oneline --grep=${i:0:7} ${1}..${2}`
 
 	for f in $fix; do
-		# did it pick already? otherwise print out the fix commit id.
+		# was it picked already? or we may need this commit.
 		gotfix=`git log --oneline --grep=${f:0:7} ${1}..`
 		[ -z "$gotfix" ] && echo $f
 	done

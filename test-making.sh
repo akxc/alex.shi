d745d8a76af3b803f43ea3c04edda68aa57a8cbf
Alex Shi <alex.shi@linaro.org>
2016-05-13 21:49:22 +0800

    test-making: gcc update
    To use gcc aarch64 5.3 and gcc arm 4.8 version

--- test-making.sh
+++ test-making.sh
@@ -1,9 +1,9 @@
-#echo -e "\nmake dove_defconfig vmlinux\n" &&
-#ARCH=arm CROSS_COMPILE=arm-unknown-linux-gnueabi- make -j 8  dove_defconfig vmlinux -s &&
 echo -e "\nmake arm64 defconfig vmlinux\n" &&
-make -s ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- -j 8 defconfig vmlinux &&
+#make -s ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- -j 8 defconfig vmlinux &&
+make -s ARCH=arm64 CROSS_COMPILE=aarch64-linux- -j 8 defconfig vmlinux &&
 make ARCH=arm64 clean; echo -e "\nmake multi_v7_defconfig vmlinux\n" &&
-ARCH=arm CROSS_COMPILE=arm-unknown-linux-gnueabi- make -j 8  multi_v7_defconfig vmlinux -s &&
+#ARCH=arm CROSS_COMPILE=arm-unknown-linux-gnueabi- make -j 8  multi_v7_defconfig vmlinux -s &&
+ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- make -j 8  multi_v7_defconfig vmlinux -s &&
 make ARCH=arm clean; echo -e "\nmake x86 defconfig vmlinux\n" &&
 make -s -j 8 defconfig vmlinux &&
 make clean; echo -e "\n FINISHED all make testing!\n"

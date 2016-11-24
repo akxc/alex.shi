1e3a6742499e07cf2dbcc97334d7bb9893be9c3f
Alex Shi <alex.shi@linaro.org>
2016-11-24 16:55:40 +0800

    is-lsk-release-week: fix bash output issue
    Like the following, it's print _\b2_\b4 instead of 24.
    So stop use 'awk'.
    
    $ bash -x /home/alexs/lsk/scripts/is-lsk-release-week.sh
    ++ date +%d
    + today=24
    +++ cal
    +++ awk '{print $5}'
    +++ grep -E '[0-9]'
    +++ tail -n 1
    ++ echo $'_\b2_\b4'
    + lastTh=$'_\b2_\b4'
    
    Signed-off-by: Alex Shi <alex.shi@linaro.org>

--- is-lsk-release-week.sh
+++ is-lsk-release-week.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 today=`date +%d`
-lastTh=`cal | awk '{print $5}' | grep -E '[0-9]' | tail -n 1`
+#maybe the awk record char as _\bx, anyway the commented line may make the $lastTh mess
+#lastTh=`cal -N |awk '/^Th/ {print $(NF)}'`
+lastTh=`cal -h|cut -c13,14|sed '/^ *$/d'|tail -1`
 
 # today is in LSK release week.
-if [ $today -gt `expr $lastTh - 4` -a $today -lt `expr $lastTh + 1` ]; then
+if [ $today -gt $(expr $lastTh - 4) -a $today -lt $(expr $lastTh + 1) ]; then
 	exit 0
 else
 	exit 1

73ae542a23adfa06894a2ab4df397f95838bf4d3
Alex Shi <alex.shi@linaro.org>
2017-01-06 11:02:20 +0800

    new scripts: search commit by subject
    same commit change it's id in backporting/cherry-pick. So we need
    to which branches picked up this commit by unchange subject.
    Usage: ./$0 'commit subject'
    
    Alex

---
+++ search-commit-subj.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#userage: ./$0 'commit subject'
+#output: list all branches which included this commit.
+
+for x in `git log --oneline --all --grep "$@" | grep "$@" | cut -d ' ' -f 1`
+do
+	echo $x
+	git br -a --contains $x
+done

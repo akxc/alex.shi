3649c357087eb7e9ed2ea792ef2167b0f7cd783e
Alex Shi <alex.shi@linaro.org>
2017-03-22 17:02:20 +0800

    chkfix: coding style and comments change
    Change some global variables to local and add/correct some comments

--- chkfix.sh
+++ chkfix.sh
@@ -1,55 +1,56 @@
 #!/bin/bash
-# parameter $1, the backport base kernel; $2, upstream branch name or tag.
-# Using on backported branch
 #
 # This script will check all cherry picked(-sx) commits id, to see if it was 
-# mentioned/fixed in later commit log -- between base kernel and latest upstream.
+# mentioned/fixed in later commit log -- between base LTS and latest upstream.
 
 export GIT_WORK_TREE=/home/alexs/lsk/kernel
 export GIT_DIR=$GIT_WORK_TREE/.git/
 UPSTREAMBR=linux
 monitors="alex.shi@linaro.org"
 
+#search fixed cid for a given topic branch
 function searchcid4topic() {
+	local topic="$1"
 	IFS=$'\n'
-	targetcids="/tmp/$TOPIC"
+	targetcids="/tmp/$topic"
 	mkdir -p "$(dirname "$targetcids")" && touch "$targetcids"
 	> $targetcids
 
-	PICKEDCID=`git log --reverse ${LTSBR}..${TOPIC} | \
+	#get all picked commits for this topic
+	pickedid=`git log --reverse ${LTSBR}..${topic} | \
 		grep 'cherry picked from commit' | awk  '{print $5}'`
 
 	local patterns;
-	for i in $PICKEDCID; do
+	for i in $pickedid; do
 		patterns="$patterns --grep=\"\<${i:0:7}\""
 	done
 
-	#[ -z "$patterns" ] && return
-
-	# get all commits which mentioned a picked commit $patterns
+	#search all commits which mentioned any picked commits in $patterns
 	echo -e "\n----------all fixing commits-----------\n" > $targetcids
 	cmd="git log --oneline --reverse ${patterns} ${LTSBR}..${UPSTREAMBR} &>> $targetcids"
 	if ! eval $cmd; then
-		echo -e "check $TOPIC failed on command \n $cmd\n" |
-				mutt -s "Failed on $TOPIC checking" $monitors
+		echo -e "check $topic failed on command \n $cmd\n" |
+				mutt -s "Failed on $topic checking" $monitors
 		return
 	fi
 	echo -e "\n----------what we missed-----------\n" >> $targetcids
 
+	#Just report omitted fixing commits, not the one we picked.
 	for f in `cat $targetcids`; do
 		# only check the fix cid which isn't in our $topic.
 		[ "${f:0:7}" = "-------" ] && continue;
-		fixed=$(git log --oneline --grep="\<${f:0:7}" ${LTSBR}..${TOPIC})
+		fixed=$(git log --oneline --grep="\<${f:0:7}" ${LTSBR}..${topic})
 		[ -z "$fixed" ] && echo $f >> $targetcids
 	done
 
-	cat $targetcids | mutt -s "$TOPIC: fix condidates" $monitors
+	cat $targetcids | mutt -s "$topic: fix condidates" $monitors
 }
 
+#check lsk version 4.1/4.4/4.9 now
 for version in 4.1 4.4 4.9; do
 LTSBR="lts/linux-${version}.y"
 	topics=`git branch -r | grep "v$version/topic/"`
-	for TOPIC in $topics; do
-		searchcid4topic
+	for t in $topics; do
+		searchcid4topic $t
 	done
 done

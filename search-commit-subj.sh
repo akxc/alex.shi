cbb80499c4548e7ef9e78269ecec3fba418169b1
Alex Shi <alex.shi@linaro.org>
2017-12-12 20:13:14 +0800

    search commit by subject: add '-F' parameter for grep
    It could will pass by special chars like '*', and also speed up
    search process.

--- search-commit-subj.sh
+++ search-commit-subj.sh
@@ -3,7 +3,7 @@
 #userage: ./$0 'commit subject'
 #output: list all branches which included this commit.
 
-for x in `git log --oneline --all --grep "$@" | grep "$@" | cut -d ' ' -f 1`
+for x in `git log --oneline --all --grep -F "$@" | grep -F "$@" | cut -d ' ' -f 1`
 do
 	echo $x
 	git br -a --contains $x
